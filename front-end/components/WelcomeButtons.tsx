49cfaf8ba0e24b90378340942c3b42b111186d94
Kim Parco <parcokim15@gmail.com>
2026-03-29 08:14:47 +0800

    Confirmed working version of front-end-kim

--- front-end/components/WelcomeButtons.tsx
+++
@@ -1,1 +0,0 @@
-export { default, TRANSLATIONS } from "./welcome/WelcomePage";
--- front-end/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface UserData {
-  username: string;
-  password: string;
-  photoUrl: string | null; // data URL from FileReader, or null if no photo
-  stars: number;
-  level: number;
-  completionRate: number;
-}
